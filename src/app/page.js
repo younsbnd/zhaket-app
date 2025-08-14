52b2036bc67038fb91658b4b1cd39250fc52eb55
arianjahangiri <arianjahangirimis@gmail.com>
2025-08-14 17:54:29 +0330

    fix:I deleted the tiptapmenuebar folder and placed the tiptop menu in the tiptop editor folder itself so that the folders are standard.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
 
-const HomePage = () => {
-  return <div></div>;
-};
-
-export default HomePage;
+import TiptapEditor from "@/components/shared/tiptapeditor/TipTap";
+import React, { useState } from "react";
+export default function Page() {
+  const [arrayValue, setArrayValue] = useState([]);
+  return (
+    <div className="p-4">
+      <TiptapEditor
+     
+        onChangeBlocks={setArrayValue}
+      />
+      <h3 className="mt-4 font-bold">خروجی آرایه:</h3>
+      <pre>{JSON.stringify(arrayValue, null, 2)}</pre>
+    </div>
+  );
+}
