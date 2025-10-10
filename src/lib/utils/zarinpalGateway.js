a2dcd989ab87e891351b83834acb5da243f1d31f
younesbnd <younesbnd20@yahoo.com>
2025-10-10 08:34:22 +0330

    feat(wallet): add wallet navigation and Zarinpal payment utility functions
    This commit introduces a new wallet navigation item in the user panel and implements utility functions for creating and verifying payments with the Zarinpal payment gateway.
    
    ### Changes:
    - **User Navigation**: Added a new entry for the wallet in the user navigation menu.
    - **Zarinpal Gateway**: Created `zarinpalCreatePayment` and `zarinpalVerifyPayment` functions to handle payment requests and verifications, enhancing the wallet's functionality.

---
+++ src/lib/utils/zarinpalGateway.js
@@ -0,0 +1,68 @@
+// create payment request with zarinpal
+export const zarinpalCreatePayment = async (
+  amountInRial,
+  description,
+  callbackUrl
+) => {
+  try {
+    const url = process.env.ZARINPAL_API_BASE_URL + "/request.json";
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        amount: amountInRial,
+        description,
+        callback_url: callbackUrl,
+        merchant_id: process.env.ZARINPAL_PAYMENT_MERCHENT_ID,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw result;
+    }
+
+    return {
+      data: result,
+      status: response.status,
+      ok: response.ok,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+// verify payment with zarinpal
+export const zarinpalVerifyPayment = async (authority, amount) => {
+  try {
+    const url = process.env.ZARINPAL_API_BASE_URL + "/verify.json";
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        authority,
+        amount,
+        merchant_id: process.env.ZARINPAL_PAYMENT_MERCHENT_ID,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw result;
+    }
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
